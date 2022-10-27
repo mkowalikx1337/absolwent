0e5d130b0bbf58bca192e2862e9226660301e6f9
Mefjuuuu <mefju.exe@gmail.com>
2022-10-27 09:40:10 +0200

    message perms system

---
+++ wiadomosci/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from wiadomosci.models import Wiadomosc
+
+
+class WiadomoscForm(forms.ModelForm):
+    class Meta:
+        model = Wiadomosc
+        fields = ('tresc', 'data_d')
+        widgets = {
+            'tresc': forms.Textarea(attrs={'cols': 100, 'rows': 2}),
+            'data_d': forms.DateTimeInput(),
+        }
--- wiadomosci/urls.py
+++ wiadomosci/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'wiadomosci'
 urlpatterns = [
    path('', views.lista_wiadomosci, name='lista'),
+   path('dodaj/', views.dodaj_wiadomosc, name='dodaj'),
 ]
